4253875b014c788db07af88f8f853fa2c1a43ffc
prince owire <princeowire131@gmail.com>
2023-01-19 12:51:37 +0100

    completed

---
+++ sammei/script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("anty-salvay")
+const open = document.getElementById("owr")
+
+open.addEventListener("click", () => {
+    menu.classList.toggle("show-nav")
+})
+
+console.log(menu)
+console.log(open)
